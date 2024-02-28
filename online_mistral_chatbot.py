2de5cd4f2b6f607d2e29505ee26029dd4a6b63fe
alaatiger989 <70290385+alaatiger989@users.noreply.github.com>
2024-02-28 14:54:52 +0200

    Update online_mistral_chatbot.py

--- online_mistral_chatbot.py
+++ online_mistral_chatbot.py
@@ -20,7 +20,7 @@ from deep_translator import GoogleTranslator
 import streamlit as st
 
 system_prompt = "You are a helpful, respectful and honest assistant. Always answer as helpfully as possible, while being safe. Your answers should not include any harmful, unethical, racist, sexist, toxic, dangerous, or illegal content. Please ensure that your responses are socially unbiased and positive in nature.If a question does not make any sense, or is not factually coherent, explain why instead of answering something not correct. If you don't know the answer to a question, please don't share false information."
-st.markdown(system_prompt)
+
 
 @st.cache_resource
 def create_chain():
